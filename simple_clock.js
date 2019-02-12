5d5a9627c873c7ad96160ce907bd5e3735213049
kenta.hatano <kenta.hatano@s-cubism.jp>
2019-02-12 12:33:16 +0900

    色を変えられるように（シンプルじゃなくなってきた・・・）

--- simple_clock.js
+++ simple_clock.js
@@ -1,15 +1,50 @@
-var LIGHTORANGE = 'rgb(255,224,192)';
+var LIGHTTANGERINE = 'rgb(255,224,160)';
+var TANGERINE = 'rgb(255,192,64)';
+var LIGHTOCEAN = 'rgb(160,224,255)';
+var OCEAN = 'rgb(64,192,255)';
+var LIGHTASH = 'rgb(192,192,192)';
+var ASH = 'rgb(128,128,128)';
+var LIGHTOLIVE = 'rgb(192,224,0)';
+var OLIVE = 'rgb(128,192,0)';
+var LIGHTVIOLET = 'rgb(224,192,224)';
+var VIOLET = 'rgb(192,128,192)';
+var LIGHTCHERRY = 'rgb(255,192,224)';
+var CHERRY = 'rgb(255,128,192)';
+
+var THEMECOLOR = [TANGERINE, OCEAN, ASH, OLIVE, VIOLET, CHERRY];
+var THEMECOLOR_LIGHT = [LIGHTTANGERINE, LIGHTOCEAN, LIGHTASH, LIGHTOLIVE, LIGHTVIOLET, LIGHTCHERRY];
+
 var LIGHTGRAY = 'rgb(192,192,192)';
-var ORANGE = 'rgb(255,192,0)';
 var DARKGRAY = 'rgb(64,64,64)';
 var DEEPDARKGRAY = 'rgb(32,32,32)';
 var RED = 'rgb(255,0,0)';
 var BLUE = 'rgb(0,0,255)';
 
-var showSecondBar = false;
+var showSecondBar = 0;
+var showDigitalClock = 0;
+var theme = 0;
 
 onload = function() {
   heartbeat();
+}
+
+onkeypress = function(e) {
+  switch (e.key) {
+    case 'c':
+      theme = (theme + 1) % (THEMECOLOR.length);
+      drawClock();
+      break;
+    case 's':
+      showSecondBar = !showSecondBar;
+      drawClock();
+      break;
+    case 'd':
+      showDigitalClock = (showDigitalClock + 1) % 3;
+      drawClock();
+      break;
+    default:
+      break;  
+  }
 }
 
 function heartbeat() {
@@ -31,6 +66,7 @@ onresize = function() {
 
 function clockClicked() {
   showSecondBar = !showSecondBar;
+  showDigitalClock = showSecondBar;
   drawClock();
 }
 
@@ -51,7 +87,8 @@ function drawClock() {
   var hour = now.getHours();
   var minute = now.getMinutes();
   var second = now.getSeconds();
-  var datetime = "" + year + "/" + month + "/" + day + " " + hour + ":" + (minute < 10 ? "0" : "") + minute + ":" + (second < 10 ? "0" : "") + second;
+  var dateString = "" + year + "/" + month + "/" + day;
+  var datetimeString = "" + year + "/" + month + "/" + day + " " + hour + ":" + (minute < 10 ? "0" : "") + minute + ":" + (second < 10 ? "0" : "") + second;
   var rad, i;
 
   var ctx = canvas.getContext('2d');
@@ -59,7 +96,7 @@ function drawClock() {
 
   for (i = 0; i < 12; i++) {
     ctx.beginPath();
-    ctx.fillStyle = LIGHTORANGE;
+    ctx.fillStyle = THEMECOLOR_LIGHT[theme];
     rad = Math.PI * 2 * (i / 12);
     ctx.arc(xPadding + (1 + Math.sin(rad) * 0.9) * weight / 2,
       yPadding + (1 - Math.cos(rad) * 0.9) * weight / 2,
@@ -76,7 +113,7 @@ function drawClock() {
 
   ctx.beginPath();
   ctx.lineWidth = tit * 2;
-  ctx.strokeStyle = ORANGE;
+  ctx.strokeStyle = THEMECOLOR[theme];;
   ctx.arc(xPadding + weight / 2, yPadding + weight / 2, weight * 0.96 / 2, 0,
     Math.PI * 2, false);
   ctx.stroke();
@@ -159,10 +196,14 @@ function drawClock() {
     tit * 2, 0, Math.PI * 2, false);
   ctx.fill();
 
-  if (width + 180 < height && showSecondBar) {
+  if (width + 180 < height && showDigitalClock > 0) {
     ctx.fillStyle = DEEPDARKGRAY;
     ctx.font = "" + tit * 6 + "px sans-serif";
     ctx.textAlign = "center";
-    ctx.fillText(datetime, width / 2, (height + weight) / 2 + 72);
+    if (showDigitalClock == 1) {
+      ctx.fillText(dateString, width / 2, (height + weight) / 2 + 72);
+    } else if (showDigitalClock == 2) {
+      ctx.fillText(datetimeString, width / 2, (height + weight) / 2 + 72);
+    }
   }
 }

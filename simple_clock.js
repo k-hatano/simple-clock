65972f216e276a5e50547383ee1591ff076f7de8
kenta.hatano <kenta.hatano@s-cubism.jp>
2019-03-04 17:19:40 +0900

    ダークモードをもうちょっと作り込む

--- simple_clock.js
+++ simple_clock.js
@@ -3,44 +3,65 @@ var THEMES = [
   { // tangerine
     spot_color: 'rgb(255,224,160)',
     edge_color: 'rgb(255,192,64)',
+    edge_shadow_color: 'rgb(192,192,192)',
+    minutehand_color: 'rgb(64,64,64)',
+    centerspot_color: 'rgb(32,32,32)',
     is_dark: false
   },
   { // ocean
     spot_color: 'rgb(160,224,255)',
     edge_color: 'rgb(64,192,255)',
+    edge_shadow_color: 'rgb(192,192,192)',
+    minutehand_color: 'rgb(64,64,64)',
+    centerspot_color: 'rgb(32,32,32)',
     is_dark: false
   },
   { // ash
     spot_color: 'rgb(192,192,192)',
     edge_color: 'rgb(128,128,128)',
+    edge_shadow_color: 'rgb(192,192,192)',
+    minutehand_color: 'rgb(64,64,64)',
+    centerspot_color: 'rgb(32,32,32)',
     is_dark: false
   },
   { // olive
     spot_color: 'rgb(192,224,0)',
     edge_color: 'rgb(128,192,0)',
+    edge_shadow_color: 'rgb(192,192,192)',
+    minutehand_color: 'rgb(64,64,64)',
+    centerspot_color: 'rgb(32,32,32)',
     is_dark: false
   },
   { // lavender
     spot_color: 'rgb(224,192,224)',
     edge_color: 'rgb(192,128,192)',
+    edge_shadow_color: 'rgb(192,192,192)',
+    minutehand_color: 'rgb(64,64,64)',
+    centerspot_color: 'rgb(32,32,32)',
     is_dark: false
   },
   { // sakura
     spot_color: 'rgb(255,192,224)',
     edge_color: 'rgb(255,128,192)',
+    edge_shadow_color: 'rgb(192,192,192)',
+    minutehand_color: 'rgb(64,64,64)',
+    centerspot_color: 'rgb(32,32,32)',
     is_dark: false
   },
   { // midnight
-    spot_color: 'rgb(128,128,128)',
-    edge_color: 'rgb(48,48,48)',
+    spot_color: 'rgb(96,96,96)',
+    edge_color: 'rgb(64,64,64)',
+    edge_shadow_color: 'rgb(24,24,24)',
+    minutehand_color: 'rgb(96,96,96)',
+    centerspot_color: 'rgb(32,32,32)',
     is_dark: true
   },
 ];
 
-var SHADOW_COLOR = 'rgb(192,192,192)';
 var SECONDHAND_COLOR = 'rgb(128,128,128)';
-var MINUTEHAND_COLOR = 'rgb(64,64,64)';
-var CENTERSPOT_COLOR = 'rgb(32,32,32)';
+
+var WHITE = 'rgb(255,255,255)';
+var BLACK = 'rgb(0,0,0)';
 
 var showSecondBar = 0;
 var showDigitalClock = 0;
@@ -53,7 +74,7 @@ onload = function() {
 onkeypress = function(e) {
   switch (e.key) {
     case 'c':
-      theme = (theme + 1) % (THEMES.length);
+      switchColor();
       drawClock();
       break;
     case 's':
@@ -98,10 +119,20 @@ function clockClicked() {
   if (radius <= 0.48) {
     showSecondBar = !showSecondBar;
   } else {
-    theme = (theme + 1) % (THEMES.length);
+    switchColor();
   }
 
   drawClock();
+}
+
+function switchColor() {
+  theme = (theme + 1) % (THEMES.length);
+
+  if (THEMES[theme].is_dark) {
+    document.bgColor = 'black';
+  } else {
+    document.bgColor = 'white';
+  }
 }
 
 function drawClock() {
@@ -128,9 +159,14 @@ function drawClock() {
   var ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, width, height);
 
+  if (THEMES[theme].is_dark) {
+    ctx.fillStyle = BLACK;
+    ctx.fillRect(0, 0, width, height);
+  }
+
   ctx.beginPath();
   ctx.lineWidth = tit * 2;
-  ctx.strokeStyle = SHADOW_COLOR;
+  ctx.strokeStyle = THEMES[theme].edge_shadow_color;
   ctx.arc(xPadding + weight / 2, yPadding + weight / 2 + tit, weight * 0.96 / 2,
     0, Math.PI * 2, false);
   ctx.stroke();
@@ -138,10 +174,10 @@ function drawClock() {
   ctx.beginPath();
   ctx.lineWidth = tit * 2;
   if (THEMES[theme].is_dark) {
-    ctx.fillStyle = THEMES[theme].edge_color;
+    ctx.strokeStyle = THEMES[theme].edge_color;
     ctx.arc(xPadding + weight / 2, yPadding + weight / 2, weight * 0.96 / 2, 0,
       Math.PI * 2, false);
-    ctx.fill();
+    ctx.stroke();
   } else {
     ctx.strokeStyle = THEMES[theme].edge_color;
     ctx.arc(xPadding + weight / 2, yPadding + weight / 2, weight * 0.96 / 2, 0,
@@ -162,19 +198,14 @@ function drawClock() {
   if (showDigitalClock > 0) {
     var x = width / 2
     var y = (height + weight) / 2 + 72
-    ctx.fillStyle = CENTERSPOT_COLOR;
     if (width + 200 > height) {
       if ((hour >= 0 && hour < 3) || (hour >= 9 && hour < 15) || (hour >= 21 && hour < 24)) {
         y = (height + weight) * 3 / 8 + yPadding / 2 + tit * 6;
       } else {
         y = (height + weight) * 1 / 8 + yPadding / 2 + tit * 6;
       }
-      if (THEMES[theme].is_dark) {
-        ctx.fillStyle = THEMES[theme].spot_color;
-      } else {
-        ctx.fillStyle = THEMES[theme].edge_color;
-      }
     }
+    ctx.fillStyle = THEMES[theme].edge_color;
     ctx.font = "" + tit * 6 + "px sans-serif";
     ctx.textAlign = "center";
     if (showDigitalClock == 1) {
@@ -186,7 +217,7 @@ function drawClock() {
   
   ctx.beginPath();
   ctx.lineWidth = tit * 2;
-  ctx.strokeStyle = SHADOW_COLOR;
+  ctx.strokeStyle = THEMES[theme].edge_shadow_color;
   ctx.moveTo(xPadding + weight / 2, yPadding + weight / 2 + tit);
   rad = Math.PI * 2 * (hour / 12 + minute / 60 / 12);
   ctx.lineTo(xPadding + (1 + Math.sin(rad) * 0.6) * weight / 2, yPadding + (1 -
@@ -195,7 +226,7 @@ function drawClock() {
 
   ctx.beginPath();
   ctx.lineWidth = tit * 1.5;
-  ctx.strokeStyle = SHADOW_COLOR;
+  ctx.strokeStyle = THEMES[theme].edge_shadow_color;
   ctx.moveTo(xPadding + weight / 2, yPadding + weight / 2 + tit);
   rad = Math.PI * 2 * (minute / 60 + second / 60 / 60);
   ctx.lineTo(xPadding + (1 + Math.sin(rad) * 0.8) * weight / 2, yPadding + (1 -
@@ -204,8 +235,8 @@ function drawClock() {
 
   if (showSecondBar) {
     ctx.beginPath();
-    ctx.lineWidth = tit;
-    ctx.strokeStyle = SHADOW_COLOR;
+    ctx.lineWidth = tit * 0.8;
+    ctx.strokeStyle = THEMES[theme].edge_shadow_color;
     ctx.moveTo(xPadding + weight / 2, yPadding + weight / 2 + tit);
     rad = Math.PI * 2 * (second / 60);
     ctx.lineTo(xPadding + (1 + Math.sin(rad) * 0.82) * weight / 2, yPadding + (1 -
@@ -214,7 +245,7 @@ function drawClock() {
   }
 
   ctx.beginPath();
-  ctx.fillStyle = SHADOW_COLOR;
+  ctx.fillStyle = THEMES[theme].edge_shadow_color;
   rad = Math.PI * 2 * (i / 12);
   ctx.arc(xPadding + weight / 2,
     yPadding + weight / 2 + tit,
@@ -223,7 +254,7 @@ function drawClock() {
 
   ctx.beginPath();
   ctx.lineWidth = tit * 2;
-  ctx.strokeStyle = MINUTEHAND_COLOR;
+  ctx.strokeStyle = THEMES[theme].minutehand_color;
   ctx.moveTo(xPadding + weight / 2, yPadding + weight / 2);
   rad = Math.PI * 2 * (hour / 12 + minute / 60 / 12);
   ctx.lineTo(xPadding + (1 + Math.sin(rad) * 0.6) * weight / 2, yPadding + (1 -
@@ -232,7 +263,7 @@ function drawClock() {
 
   ctx.beginPath();
   ctx.lineWidth = tit * 1.5;
-  ctx.strokeStyle = MINUTEHAND_COLOR;
+  ctx.strokeStyle = THEMES[theme].minutehand_color;
   ctx.moveTo(xPadding + weight / 2, yPadding + weight / 2);
   rad = Math.PI * 2 * (minute / 60 + second / 60 / 60);
   ctx.lineTo(xPadding + (1 + Math.sin(rad) * 0.8) * weight / 2, yPadding + (1 -
@@ -241,11 +272,11 @@ function drawClock() {
 
   if (showSecondBar) {
     ctx.beginPath();
-    ctx.lineWidth = tit;
+    ctx.lineWidth = tit * 0.8;
     if (hour < 12) {
       ctx.strokeStyle = THEMES[theme].edge_color;
     } else {
-      ctx.strokeStyle = SECONDHAND_COLOR;
+      ctx.strokeStyle = THEMES[theme].minutehand_color;
     }
     ctx.moveTo(xPadding + weight / 2, yPadding + weight / 2);
     rad = Math.PI * 2 * (second / 60);
@@ -255,7 +286,7 @@ function drawClock() {
   }
 
   ctx.beginPath();
-  ctx.fillStyle = CENTERSPOT_COLOR;
+  ctx.fillStyle = THEMES[theme].centerspot_color;
   rad = Math.PI * 2 * (i / 12);
   ctx.arc(xPadding + weight / 2,
     yPadding + weight / 2,
